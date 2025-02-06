0a0263af3e8783cf2b68c9f3b7227083fe2ee689
Bigbinary <bigbinary@Bigbinarys-MacBook-Pro-6.local>
2025-02-06 10:35:32 +0530

    Added add to cart feature on the product details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { PageNotFound } from "components/commons";
 import Product from "components/Product";
 import ProductList from "components/ProductList";
@@ -5,32 +7,37 @@ import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 import routes from "routes";
 
 import "./App.css";
+import CartItemsContext from "./contexts/CartItemsContext";
 // eslint-disable-next-line import/extensions
 
-const App = () => (
-  <>
-    <div className="flex space-x-2">
-      <NavLink exact activeClassName="underline font-bold" to="/">
-        Home
-      </NavLink>
-      <NavLink exact activeClassName="underline font-bold" to="/product">
-        Product
-      </NavLink>
-    </div>
-    {/* <Switch>
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  return (
+    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
+      <div className="flex space-x-2">
+        <NavLink exact activeClassName="underline font-bold" to="/">
+          Home
+        </NavLink>
+        <NavLink exact activeClassName="underline font-bold" to="/product">
+          Product
+        </NavLink>
+      </div>
+      {/* <Switch>
       <Route exact component={Product} path="/product" />
       <Route exact component={ProductList} path="/products" />
       <Route exact component={Product} path="/products/:slug" />
       <Redirect exact from="/" to="/products" />
       <Route component={PageNotFound} path="*" />
-    </Switch> */}
-    <Switch>
-      <Route exact component={Product} path={routes.products.show} />
-      <Route exact component={ProductList} path={routes.products.index} />
-      <Redirect exact from={routes.root} to={routes.products.index} />
-      <Route component={PageNotFound} path="*" />
-    </Switch>
-  </>
-);
+      </Switch> */}
+      <Switch>
+        <Route exact component={Product} path={routes.products.show} />
+        <Route exact component={ProductList} path={routes.products.index} />
+        <Redirect exact from={routes.root} to={routes.products.index} />
+        <Route component={PageNotFound} path="*" />
+      </Switch>
+    </CartItemsContext.Provider>
+  );
+};
 
 export default App;
