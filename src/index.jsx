5a07d5caf82cd53675b69e40c150b57ac4e094f0
BigBinary <bigbinary@BigBinarys-MacBook-Pro.local>
2025-02-07 18:54:22 +0530

    Initialized query client instance

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import initializeAxios from "apis/axios";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 // i18n should load before App initialization
 // eslint-disable-next-line import/order, no-unused-vars
 import "components/commons/i18n";
+import queryClient from "utils/queryClient";
+
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -15,10 +18,12 @@ initializeAxios();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ToastContainer />
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ToastContainer />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
