9782e996224f975095693f770498c882f54e8450
SumitPanda0 <sumit.panda@bigbinary.com>
2025-02-07 11:49:31 +0530

    Initialized i18n

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,9 @@ import initializeAxios from "apis/axios";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-
+// i18n should load before App initialization
+// eslint-disable-next-line import/order, no-unused-vars
+import "components/commons/i18n";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
