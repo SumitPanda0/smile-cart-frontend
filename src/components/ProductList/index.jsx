92eac9d9807ebdb5395000106b059e5d58c96b52
Sumit Panda <bigbinary@BigBinarys-MacBook-Pro.local>
2025-02-09 12:32:37 +0530

    Added pagination to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,22 +4,28 @@ import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty } from "ramda";
 import { useTranslation } from "react-i18next";
 import withTitle from "utils/withTitle";
 
+import { DEFAULT_PAGE_SIZE, DEFAULT_PAGE_INDEX } from "./constant";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   // const [products, setProducts] = useState([]);
   // const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
   const [searchKey, setSearchKey] = useState("");
   const debouncedSearchKey = useDebounce(searchKey);
-  const { t } = useTranslation();
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
+  const productsParams = {
     searchTerm: debouncedSearchKey,
-  });
+    page: currentPage,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
+  const { t } = useTranslation();
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts(productsParams);
   // const fetchProducts = async () => {
   //   try {
   //     const { products } = await productsApi.fetch({
@@ -45,30 +51,43 @@ const ProductList = () => {
 
   // console.log(products)
   return (
-    <div className="flex h-screen flex-col">
-      <Header
-        shouldShowBackButton={false}
-        title={t("title")}
-        actionBlock={
-          <Input
-            placeholder={t("searchProducts")}
-            prefix={<Search />}
-            type="search"
-            value={searchKey}
-            onChange={e => setSearchKey(e.target.value)}
-          />
-        }
-      />
-      {isEmpty(products) ? (
-        <NoData className="h-full w-full" title={t("noData")} />
-      ) : (
-        <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
-          {products.map(product => (
-            <ProductListItem key={product.slug} {...product} />
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <div className="flex h-screen flex-col">
+        <Header
+          shouldShowBackButton={false}
+          title={t("title")}
+          actionBlock={
+            <Input
+              placeholder={t("searchProducts")}
+              prefix={<Search />}
+              type="search"
+              value={searchKey}
+              onChange={e => {
+                setSearchKey(e.target.value);
+                setCurrentPage(DEFAULT_PAGE_INDEX);
+              }}
+            />
+          }
+        />
+        {isEmpty(products) ? (
+          <NoData className="h-full w-full" title={t("noData")} />
+        ) : (
+          <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
+            {products.map(product => (
+              <ProductListItem key={product.slug} {...product} />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={page => setCurrentPage(page)}
+          pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
+      </div>
+    </>
   );
 };
 
