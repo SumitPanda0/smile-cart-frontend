882d24e18938ffd66e8b6ebb921f1db21f4613dc
Sumit Panda <bigbinary@BigBinarys-MacBook-Pro.local>
2025-02-09 19:39:32 +0530

    Implemented yup validation for checkout form

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,35 @@
+import { t } from "i18next";
+import * as yup from "yup";
+
 export const VALID_COUNT_REGEX = /^(?:\d*|)$/;
-// src/components/constants.js
 export const MRP = "mrp";
 
 export const OFFER_PRICE = "offerPrice";
+
+export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  email: yup
+    .string()
+    .email(t("validations.emailInvalid"))
+    .required(t("validations.emailRequired")),
+  country: yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+      code: yup.string().required(),
+    })
+    .nullable(),
+  firstName: yup.string().required(t("validations.firstNameRequired")),
+  lastName: yup.string().required(t("validations.lastNameRequired")),
+  address: yup.string().required(t("validations.addressRequired")),
+  apartment: yup.string().required(t("validations.apartmentRequired")),
+  city: yup.string().required(t("validations.cityRequired")),
+  state: yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+      code: yup.string().required(),
+    })
+    .nullable()
+    .required(t("validations.stateRequired")),
+  zipCode: yup.number().required(t("validations.zipCodeRequired")),
+});
